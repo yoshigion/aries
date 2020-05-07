25a415d450b9d138fd0f596121fff0e8f99454ba
Yoshimasa Obayashi <obayashi.yoshimasa@socionext.com>
2020-05-07 10:06:20 +0900

    aries: correct to use Buefy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import Buefy from 'buefy' // 追記
 import App from './App.vue'
 import router from './router'
+import 'buefy/dist/buefy.css' // 追記
 
 import Amplify, * as AmplifyModules from 'aws-amplify' // 追記
 import { AmplifyPlugin } from 'aws-amplify-vue' // 追記
@@ -8,6 +10,7 @@ import aws_exports from './aws-exports' // 追記
 Amplify.configure(aws_exports) // 追記
 
 Vue.use(AmplifyPlugin, AmplifyModules) // 追記
+Vue.use(Buefy) // 追記
 
 Vue.config.productionTip = false
 
